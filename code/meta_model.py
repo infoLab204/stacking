af1b5be78c30594a96f902d06ce47f9c63ad5fcc
infoLab204 <63629577+infoLab204@users.noreply.github.com>
2024-03-15 16:32:05 +0900

    Add files via upload

---
+++ code/meta_model.py
@@ -0,0 +1,70 @@
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras import optimizers
+from tensorflow.keras.models import Model
+import numpy as np
+import pandas as pd
+from scipy import stats
+from keras.callbacks import EarlyStopping
+import os
+import sys
+
+from sklearn.model_selection import train_test_split
+
+
+# CV stacking : train_predicted.txt
+y_pheno=pd.read_csv("y_tr_output_full", sep="\n", header=None)
+
+rrBLUP_pred=pd.read_csv("rrBLUP_yHat_train_stacking_predicted.txt", sep="\n",header=None) 
+BA_pred=pd.read_csv("BayesA_yHat_train_stacking_predicted.txt", sep="\n",header=None) 
+BB_pred=pd.read_csv("BayesB_yHat_train_stacking_predicted.txt", sep="\n",header=None) 
+BC_pred=pd.read_csv("BayesC_yHat_train_stacking_predicted.txt", sep="\n",header=None) 
+BL_pred=pd.read_csv("BL_yHat_train_stacking_predicted.txt", sep="\n",header=None) 
+BRR_pred=pd.read_csv("BRR_yHat_train_stacking_predicted.txt", sep="\n",header=None) 
+
+train_n=int(y_pheno.shape[0]*0.8)
+
+# CV stacking : test_predicted.txt
+rrBLUP_test_pred=pd.read_csv("rrBLUP_yHat_test_stacking_predicted.txt", sep="\n",header=None) 
+BA_test_pred=pd.read_csv("BayesA_yHat_test_stacking_predicted.txt", sep="\n",header=None) 
+BB_test_pred=pd.read_csv("BayesB_yHat_test_stacking_predicted.txt", sep="\n",header=None) 
+BC_test_pred=pd.read_csv("BayesC_yHat_test_stacking_predicted.txt", sep="\n",header=None) 
+BL_test_pred=pd.read_csv("BL_yHat_test_stacking_predicted.txt", sep="\n",header=None) 
+BRR_test_pred=pd.read_csv("BRR_yHat_test_stacking_predicted.txt", sep="\n",header=None) 
+
+Stack_final_X_train=np.concatenate((rrBLUP_pred, BA_pred, BB_pred, BC_pred, BL_pred, BRR_pred), axis=1)
+Stack_final_X_test=np.concatenate((rrBLUP_test_pred, BA_test_pred, BB_test_pred, BC_test_pred, BL_test_pred, BRR_test_pred),axis=1)
+
+y_pheno=y_pheno.values
+y_train_full=y_pheno[:train_n]
+y_test=y_pheno[train_n:]
+
+Epoch=[400]
+
+for i in Epoch :
+    inputs=Input(shape=(6,))
+    hidden=Dense(12,activation='sigmoid')(inputs)
+    outputs=Dense(1,activation='linear')(hidden)
+
+    E=i
+    BS=48
+
+    linear_model=Model(inputs, outputs)
+
+    adam=optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    linear_model.compile(optimizer=adam, loss='mse')
+
+    hist=linear_model.fit(Stack_final_X_train,y_train_full,epochs=E, batch_size=BS, verbose=1, validation_split=0.2)
+
+    test_pred=linear_model.predict(Stack_final_X_test)
+    np.savetxt(f"Stacking_predicted_test.txt",test_pred, fmt="%.8f")
+    train_pred=linear_model.predict(Stack_final_X_train)
+    np.savetxt(f"Stacking_predicted_train.txt",train_pred, fmt="%.8f")
+
+    co1,p1=stats.pearsonr(y_test[:,0], test_pred[:,0])
+    co1_1,p1_1=stats.pearsonr(y_train_full[:,0], train_pred[:,0])
+
+
+    meta_cor=f"Stacking_NN_correlation.txt"
+    outfile=open(meta_cor,"w")
+    outfile.write(f"stacking_test {co1} stacking_train {co1_1}\n")
+    outfile.close()
---
+++ code/stacking_base_model.R
@@ -0,0 +1,160 @@
+library(BGLR)
+library(rrBLUP)
+
+SNP <- read.table('X_tr_output_full',sep="\t", header=F)
+Y0 <- read.table('y_tr_output_full',sep="\t", header=F)
+
+n_fold=5
+p<-ncol(SNP)
+print(p)
+
+SNP <- SNP[,1:(p-1)]
+
+n<-nrow(SNP)
+train_n=as.integer(n*0.8)
+cat(n, train_n)
+
+SNP0 <- SNP[1:train_n,]
+print(nrow(SNP0))
+y<-Y0[1:train_n,1]
+print(y)
+
+split_n=as.integer(train_n/n_fold)
+print(split_n)
+
+rrBLUP_yHat<-c(1:train_n)
+BA_yHat<-c(1:train_n)
+BB_yHat<-c(1:train_n)
+BC_yHat<-c(1:train_n)
+BL_yHat<-c(1:train_n)
+BRR_yHat<-c(1:train_n)
+
+test_idx=as.integer(seq(1,train_n, by=split_n))
+
+for(i in 1:n_fold) {
+    if(i<n_fold) test<-c(test_idx[i]:(test_idx[i+1]-1))
+    else test<-c(test_idx[i]:train_n)
+
+    ## rrBLUP
+    pheno_train <- y[-test]
+    pheno_test <- y[test]
+    geno_train <- as.matrix(SNP0[-test,])
+    geno_test <- as.matrix(SNP0[test,])
+
+    rrblupmodel <- mixed.solve(y=pheno_train, Z=geno_train, K=NULL, X=NULL)
+    marker_effects <- rrblupmodel$u
+    rrBLUE <- as.vector(rrblupmodel$beta)
+
+    rrblup_predicted_test <- geno_test %*% marker_effects
+    rrblup_predicted_result <-as.vector(rrblup_predicted_test[,1])+rrBLUE
+
+    rrblup_predicted_train <- geno_train %*% marker_effects
+    rrblup_predicted_train_result <-as.vector(rrblup_predicted_train[,1])+rrBLUE
+    
+    rrBLUP_yHat[test] <- rrblup_predicted_result
+
+    write.table(rrBLUP_yHat,file="rrBLUP_yHat_train_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+
+    yNA <- y
+    yNA[test] <-NA
+
+    ## BayesA
+    ETA <-list(list(X=SNP0, model="BayesA"))
+    saveAt=paste0("BayesA_",i,"_")
+    fmBA <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt=saveAt)
+    BA_yHat[test]=fmBA$yHat[test]
+
+    write.table(BA_yHat,file="BayesA_yHat_train_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+  
+    ## BayesB
+    ETA <-list(list(X=SNP0, model="BayesB"))
+    saveAt=paste0("BayesB_",i,"_")
+    fmBB <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt=saveAt)
+    BB_yHat[test]=fmBB$yHat[test]
+
+    write.table(BB_yHat,file="BayesB_yHat_train_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+
+    ## BayesC
+    ETA <-list(list(X=SNP0, model="BayesC"))
+    saveAt=paste0("BayesC_",i,"_")
+    fmBC <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt=saveAt)
+    BC_yHat[test]=fmBC$yHat[test]
+
+    write.table(BC_yHat,file="BayesC_yHat_train_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+
+    ## Bayes LASSO
+    ETA <-list(list(X=SNP0, model="BL"))
+    saveAt=paste0("BL_",i,"_")
+    fmBL <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt=saveAt)
+    BL_yHat[test]=fmBL$yHat[test]
+
+    write.table(BL_yHat,file="BL_yHat_train_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+    ## Bayes GBLUP
+    ETA <-list(list(X=SNP0, model="BRR"))
+    saveAt=paste0("BRR_",i,"_")
+    fmBRR <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt=saveAt)
+    BRR_yHat[test]=fmBRR$yHat[test]
+
+    write.table(BRR_yHat,file="BRR_yHat_train_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+}
+print("train learining END")
+
+
+## rrBLUP test set predict
+rrBLUP_yHat_test<-c(1:train_n)
+test <-c((train_n+1):n)
+y<-c(1:n)
+train_idx=seq(1,train_n)
+y<-Y0[,1]
+
+pheno_train <- y[-test]
+pheno_test <- y[test]
+geno_train <- as.matrix(SNP[-test,])
+geno_test <- SNP[test,]
+geno_test <- as.matrix(geno_test)
+
+rrblupmodel <- mixed.solve(y=pheno_train, Z=geno_train, K=NULL, X=NULL)
+marker_effects <- rrblupmodel$u
+rrBLUE <- as.vector(rrblupmodel$beta)
+
+rrblup_predicted_test <- geno_test %*% marker_effects
+rrblup_predicted_result <-as.vector(rrblup_predicted_test[,1])+rrBLUE
+
+rrBLUP_yHat_test[test] <- rrblup_predicted_result
+write.table(rrBLUP_yHat_test[test],file="rrBLUP_yHat_test_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+test <-c((train_n+1):n)
+y<-Y0[,1]
+yNA <- y
+yNA[test] <-NA
+
+## BayesA test set predict
+ETA <-list(list(X=SNP, model="BayesA"))
+fmBA <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt="BayesA_")
+write.table(fmBA$yHat[test],file="BayesA_yHat_test_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+## BayesB test set predict
+ETA <-list(list(X=SNP, model="BayesB"))
+fmBB <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt="BayesB_")
+write.table(fmBB$yHat[test],file="BayesB_yHat_test_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+## BayesC test set predict
+ETA <-list(list(X=SNP, model="BayesC"))
+fmBC <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt="BayesC_")
+write.table(fmBC$yHat[test],file="BayesC_yHat_test_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+## Bayes Lasso test set predict
+ETA <-list(list(X=SNP, model="BL"))
+fmBL <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt="BL_")
+write.table(fmBL$yHat[test],file="BL_yHat_test_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
+
+## gBLUP test set predict
+ETA <-list(list(X=SNP, model="BRR"))
+fmBRR <-BGLR(y=yNA, ETA=ETA, nIter=5000, burnIn=1000, saveAt="BRR_")
+write.table(fmBRR$yHat[test],file="BRR_yHat_test_stacking_predicted.txt",row.names=F,col.names=F,quote=F, append=F,sep="\n")
